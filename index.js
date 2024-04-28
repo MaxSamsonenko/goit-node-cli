ff09c05f36288e0078b9ed88df343d614a718cae
Max Samsonenko <samsonenko.maxwork@gmail.com>
2024-04-28 21:05:06 +0300

    save

---
+++ index.js
@@ -0,0 +1,46 @@
+import { program } from "commander";
+import {
+	listContacts,
+	addContact,
+	removeContact,
+	getContactById,
+} from "./contacts.js";
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+	switch (action) {
+		case "list":
+			const contacts = await listContacts();
+			console.table(contacts);
+			break;
+
+		case "get":
+			const contact = await getContactById(id);
+			console.log(contact);
+			break;
+
+		case "add":
+			const newContact = await addContact(name, email, phone);
+			console.log(newContact);
+			break;
+
+		case "remove":
+			const contactToRemove = await removeContact(id);
+			console.log(contactToRemove);
+			break;
+
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
+}
+
+invokeAction(options);
